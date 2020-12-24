6036d03854d9e20e665d9bcb0480b89af11e6ae1
Regina <g.regina28@gmail.com>
2020-12-24 17:40:44 +0500

    Added login form

---
+++ VK Client1/VK Client/LoginFormViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginFormViewController.swift
+//  VK Client
+//
+//  Created by Regina Galishanova on 19.12.2020.
+//
+
+import UIKit
+
+class LoginFormViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapOnScroll))
+        scrollView?.addGestureRecognizer(tapGesture)
+        scrollView.isUserInteractionEnabled = true
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func keyboardWillShow(notification: Notification) {
+        guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.size.height, right: 0)
+        scrollView.contentInset = insets
+    }
+    @objc func keyboardWillHide(notification: Notification) {
+        let insets = UIEdgeInsets.zero
+        scrollView.contentInset = insets
+    }
+    @objc func didTapOnScroll() {
+        self.scrollView?.endEditing(true)
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+}
